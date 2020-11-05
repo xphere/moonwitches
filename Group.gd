f95767bab3a041f8c9c6762f47d2583838945243
Berny Cantos <be@rny.cc>
2020-11-05 15:57:15 +0100

    can't group if too far or blocked

--- Group.gd
+++ Group.gd
@@ -1,5 +1,7 @@
 extends Node
 
+const GROUP_DISTANCE := 24.0
+
 export(bool) var together := false
 
 
@@ -20,5 +22,25 @@ func set_together(value: bool) -> void:
 
 
 func _input(event) -> void:
-	if event.is_action_pressed("change_group"):
+	if event.is_action_pressed("change_group") and _can_change():
 		set_together(not together)
+
+
+func _can_change() -> bool:
+	if together:
+		return true
+
+	var from : Vector2 = $Mentor.global_position
+	var to : Vector2 = $Pupil.global_position
+	if from.distance_to(to) > GROUP_DISTANCE:
+		return false
+
+	var _together : RayCast2D = $Together
+	_together.enabled = true
+	_together.global_position = from
+	_together.cast_to = to - from
+	_together.force_raycast_update()
+	var is_colliding : bool = _together.is_colliding()
+	_together.enabled = false
+
+	return not is_colliding
