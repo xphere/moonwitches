525b972c5f55451d8e626689b22b522a9dafefc0
Berny Cantos <be@rny.cc>
2020-11-05 11:53:45 +0100

    implement group/ungroup

--- FollowMentor.gd
+++ FollowMentor.gd
@@ -4,9 +4,6 @@ export(NodePath) var following
 
 
 func get_movement() -> Vector2:
-	if not following:
-		return $Controller.get_movement()
-
 	var destination := (get_node(following) as Node2D).global_position
 	var path : Vector2 = destination - get_parent().global_position
 
--- MainCharacter.gd
+++ MainCharacter.gd
@@ -1,9 +1,28 @@
 extends KinematicBody2D
 
-export(float) var speed := 32.0
+export(float) var max_speed := 1200.0
+export(NodePath) var controller
+
+var _current_speed
+var _controller
+
+
+func _ready() -> void:
+	_current_speed = max_speed
+	set_controller(controller)
 
 
 func _physics_process(_delta: float) -> void:
-	var movement : Vector2 = $Controller.get_movement()
+	var movement : Vector2 = _controller.get_movement()
 	if movement:
-		movement = move_and_slide(movement * _delta * speed)
+		movement = move_and_slide(movement * _delta * _current_speed)
+
+
+func set_speed(speed: float) -> void:
+	_current_speed = speed
+
+
+func set_controller(path: NodePath) -> void:
+	print(self, ' set controller to ', get_node(path), ' (', path, ')')
+	controller = path
+	_controller = get_node(controller)
