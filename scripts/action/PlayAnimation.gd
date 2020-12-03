fbb8cd484d408176e4b9da643d5b474f67d37b80
Berny Cantos <be@rny.cc>
2020-12-03 16:29:56 +0100

    node reorder and id juggling

--- scripts/action/PlayAnimation.gd
+++ scripts/action/PlayAnimation.gd
@@ -10,6 +10,7 @@ export var track_name : String
 func execute() -> void:
 	var animation := get_node(animation_path) as AnimationPlayer
 
+	animation.stop()
 	animation.play(track_name)
 	if wait:
 		animation.connect("finished", self, "_on_animation_finished")
